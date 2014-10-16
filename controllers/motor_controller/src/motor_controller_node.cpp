7ad856d6da0861201ab022d55aca8e42a3849916
ras <ras@nuc.(none)>
2014-10-16 16:37:44 +0200

    first implementation of full PID

--- controllers/motor_controller/src/motor_controller_node.cpp
+++ controllers/motor_controller/src/motor_controller_node.cpp
@@ -4,6 +4,7 @@
 #include <ras_arduino_msgs/Encoders.h>
 #include <geometry_msgs/Twist.h>
 #include <math.h>
+#include <ctime>
 
 class MotorControllerNode
 {
@@ -14,6 +15,8 @@ public:
   ros::Publisher pub;
   ros::Subscriber encode;
   ros::Subscriber twist;
+  std::list pid;
+
   
   MotorControllerNode()
   {
@@ -31,6 +34,7 @@ public:
     pub = n.advertise<ras_arduino_msgs::PWM>("/arduino/pwm", 1000);
     encode = n.subscribe("/arduino/encoders", 1000, &MotorControllerNode::encCallback, this);
     twist = n.subscribe("/motor_controller/twist", 1000, &MotorControllerNode::twistCallback, this);
+
   }
   void encCallback(const ras_arduino_msgs::Encoders::ConstPtr &msg)
   {
@@ -43,28 +47,57 @@ public:
     twi.angular.z = msg->angular.z;
   }
   
+
+
+
   void calc()
   {
+
+
+    n.getParam("/settings/pid/Kp1", Kp1);
+    n.getParam("/settings/pid/Ki1", Ki1);
+    n.getParam("/settings/pid/Kd1", Kd1);
+    n.getParam("/settings/pid/Kp2", Kp2);
+    n.getParam("/settings/pid/Ki2", Ki2);
+    n.getParam("/settings/pid/Kd2", Kd2);
+
+
+    double time_diff_ms = (std::clock() - previous_time) / (double)(CLOCKS_PER_SEC / 1000);
+    previous_time = std::clock();
+
     double desired_w1 = (twi.linear.x-(0.5*0.238*twi.angular.z))/(0.0975/2);
     double desired_w2 = (twi.linear.x+(0.5*0.238*twi.angular.z))/(0.0975/2);
     double estimated_w1 = ((double) (enc.delta_encoder1)*2*M_PI*10)/360;
     double estimated_w2 = ((double) (enc.delta_encoder2)*2*M_PI*10)/360;
-    pwm.PWM1 = pwm.PWM1 + (int)(1*(desired_w1 - estimated_w1));
-    pwm.PWM2 = -(-pwm.PWM2 + (int)(1*(desired_w2 - estimated_w2)));
+    double error1 = desired_w1 - estimated_w1;
+    double error2 = desired_w2 - estimated_w2;
+    double D1 = (error1 - last_error1)/ time_diff_ms;
+    double D2 = (error2 - last_error2)/ time_diff_ms;
+    double I1 = I1 + Ki1*(error1*time_diff_ms);
+    double I2 = I2 + Ki2*(error2*time_diff_ms);
+    pwm.PWM1 = pwm.PWM1 + (int)(Kp1*error1) + (int) (Kd1*D1) + (int) I1;
+    pwm.PWM2 = -(-pwm.PWM2 + (int)(Kp2*error2) + (int) (Kd2*D2) + (int) I2);
+    last_error1 = error1;
+    last_error2 = error2;
     
-    ras_arduino_msgs::PWM pwm_msg;
+
+    /*ras_arduino_msgs::PWM pwm_msg;
     pwm_msg = pwm;
     if(pwm_msg.PWM1 < 30) pwm_msg.PWM1 = 0;
     if(pwm_msg.PWM2 > -30) pwm_msg.PWM2 = 0;
-    pub.publish(pwm_msg);
+    pub.publish(pwm_msg);*/
     ROS_INFO("[%f, %f] estimated: [%f, %f], PWM: [%d, %d]",desired_w1, desired_w2, estimated_w1, estimated_w2, pwm.PWM1, pwm.PWM2);
   }
   
 private:
+  double last_error1 = 0;
+  double last_error2 = 0;
+  double I1 = 0;
+  double I2 = 0;
   ras_arduino_msgs::PWM pwm;
   ras_arduino_msgs::Encoders enc;
   geometry_msgs::Twist twi;
-  
+  std::clock_t previous_time = std::clock();
 };
 int main (int argc, char **argv){
   ros::init(argc, argv, "motor_controller_node");
