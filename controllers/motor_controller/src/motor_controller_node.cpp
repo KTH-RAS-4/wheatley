669bd6d368a49451615107d4cd8a16c69707637c
ras <ras@nuc.(none)>
2014-10-17 00:27:34 +0200

    tested quite nicely working PID (2,20,0.1)
    moved the PID variables into a Motor class

--- controllers/motor_controller/src/motor_controller_node.cpp
+++ controllers/motor_controller/src/motor_controller_node.cpp
@@ -3,15 +3,49 @@
 #include <ras_arduino_msgs/PWM.h>
 #include <ras_arduino_msgs/Encoders.h>
 #include <geometry_msgs/Twist.h>
+#include <std_msgs/Float64.h>
 #include <math.h>
+
+template <typename T> T CLAMP(const T& value, const T& low, const T& high)
+{
+  return value < low ? low : (value > high ? high : value);
+}
+
+class Motor
+{
+public:
+    double kP;
+    double kI;
+    double kD;
+    double I;
+    double last_error;
+
+    Motor()
+        : kP(0), kI(0), kD(0), I(0)
+        , last_error(0)
+    {
+    }
+
+    int update(double elapsed, double error)
+    {
+        double D = (error - last_error) / elapsed;
+        I += error*elapsed;
+        last_error = error;
+        return (int) (kP*error + kD*D + kI*I);
+    }
+};
 
 class MotorControllerNode
 {
-  
 public:
 
   ros::NodeHandle n;
-  ros::Publisher pub;
+  ros::Publisher pwmPub;
+  ros::Publisher desiredAngularVelocityLeftPub;
+  ros::Publisher desiredAngularVelocityRightPub;
+  ros::Publisher measuredAngularVelocityLeftPub;
+  ros::Publisher measuredAngularVelocityRightPub;
+
   ros::Subscriber encode;
   ros::Subscriber twist;
 
@@ -31,13 +65,14 @@ public:
   
   void init()
   {
-    pub = n.advertise<ras_arduino_msgs::PWM>("/arduino/pwm", 1000);
+    pwmPub = n.advertise<ras_arduino_msgs::PWM>("/arduino/pwm", 1000);
+    desiredAngularVelocityLeftPub   = n.advertise<std_msgs::Float64>("/motors/left/desiredAngular",   1);
+    desiredAngularVelocityRightPub  = n.advertise<std_msgs::Float64>("/motors/right/desiredAngular",  1);
+    measuredAngularVelocityLeftPub  = n.advertise<std_msgs::Float64>("/motors/left/measuredAngular",  1);
+    measuredAngularVelocityRightPub = n.advertise<std_msgs::Float64>("/motors/right/measuredAngular", 1);
+
     encode = n.subscribe("/arduino/encoders", 1000, &MotorControllerNode::encCallback, this);
     twist = n.subscribe("/motor_controller/twist", 1000, &MotorControllerNode::twistCallback, this);
-    last_error1 = 0;
-    last_error2 = 0;
-    I1 = 0;
-    I2 = 0;
   }
   void encCallback(const ras_arduino_msgs::Encoders::ConstPtr &msg)
   {
@@ -63,59 +98,57 @@ public:
 
   void calc()
   {
-    //TODO: use right for one of the motors
-    n.getParam("/settings/pid/left/P", Kp1);
-    n.getParam("/settings/pid/left/I", Ki1);
-    n.getParam("/settings/pid/left/D", Kd1);
-    n.getParam("/settings/pid/left/P", Kp2);
-    n.getParam("/settings/pid/left/I", Ki2);
-    n.getParam("/settings/pid/left/D", Kd2);
-
-    pwm.PWM1 = 0;
-    pwm.PWM2 = 0;
+    n.getParam("/settings/pid/left/P", motorL.kP);
+    n.getParam("/settings/pid/left/I", motorL.kI);
+    n.getParam("/settings/pid/left/D", motorL.kD);
+    n.getParam("/settings/pid/right/P", motorR.kP);
+    n.getParam("/settings/pid/right/I", motorR.kI);
+    n.getParam("/settings/pid/right/D", motorR.kD);
 
     double elapsed = loop_rate.expectedCycleTime().toSec();
+    double wDesiredL = (twi.linear.x-(0.5*0.238*twi.angular.z))/(0.0975/2);
+    double wDesiredR = (twi.linear.x+(0.5*0.238*twi.angular.z))/(0.0975/2);
+    double wMeasuredL = ((double) (enc.delta_encoder1)*2*M_PI*10)/360;
+    double wMeasuredR = ((double) (enc.delta_encoder2)*2*M_PI*10)/360;
+    double errorL = wDesiredL - wMeasuredL;
+    double errorR = wDesiredR - wMeasuredR;
 
-    double desired_w1 = (twi.linear.x-(0.5*0.238*twi.angular.z))/(0.0975/2);
-    double desired_w2 = (twi.linear.x+(0.5*0.238*twi.angular.z))/(0.0975/2);
-    double estimated_w1 = ((double) (enc.delta_encoder1)*2*M_PI*10)/360;
-    double estimated_w2 = ((double) (enc.delta_encoder2)*2*M_PI*10)/360;
-    double error1 = desired_w1 - estimated_w1;
-    double error2 = desired_w2 - estimated_w2;
-    double D1 = (error1 - last_error1)/ elapsed;
-    double D2 = (error2 - last_error2)/ elapsed;
-    double I1 = I1 + Ki1*(error1*elapsed);
-    double I2 = I2 + Ki2*(error2*elapsed);
-    pwm.PWM1 = (int)(Kp1*error1) + (int) (Kd1*D1) + (int) I1;
-    pwm.PWM2 = -((int)(Kp2*error2) + (int) (Kd2*D2) + (int) I2);
-    last_error1 = error1;
-    last_error2 = error2;
+    pwm.PWM1 =   motorL.update(elapsed, errorL);
+    pwm.PWM2 = - motorR.update(elapsed, errorR);
 
-    /*ROS_INFO("P: %.2f", Kp1);
-    ROS_INFO("I: %.2f", Ki1);
-    ROS_INFO("D: %.2f", Kd1);*/
+    pwm.PWM1 = CLAMP(pwm.PWM1, -255, 255);
+    pwm.PWM2 = CLAMP(pwm.PWM2, -255, 255);
+
+    /*ROS_INFO("P: %.2f", motorR.P);
+    ROS_INFO("I: %.2f", motorR.I);
+    ROS_INFO("D: %.2f", motorR.D);*/
 
     /*ras_arduino_msgs::PWM pwm_msg;
     pwm_msg = pwm;
     if(pwm_msg.PWM1 < 30) pwm_msg.PWM1 = 0;
     if(pwm_msg.PWM2 > -30) pwm_msg.PWM2 = 0;*/
-    pub.publish(pwm);
-    ROS_INFO("desired: [%6.1f, %6.1f]", desired_w1, desired_w2);
-    ROS_INFO(" actual: [%6.1f, %6.1f]", estimated_w1, estimated_w2);
-    ROS_INFO("    PWM: [%6d, %6d]", pwm.PWM1, pwm.PWM2);
+
+    pwmPub.publish(pwm);
+    std_msgs::Float64 dw1m; dw1m.data = wDesiredL;
+    std_msgs::Float64 dw2m; dw2m.data = wDesiredR;
+    std_msgs::Float64 mw1m; mw1m.data = wMeasuredL;
+    std_msgs::Float64 mw2m; mw2m.data = wMeasuredR;
+    desiredAngularVelocityLeftPub.publish(dw1m);
+    desiredAngularVelocityRightPub.publish(dw2m);
+    measuredAngularVelocityLeftPub.publish(mw1m);
+    measuredAngularVelocityRightPub.publish(mw2m);
+
+    ROS_INFO(" desired: [%6.1f, %6.1f]", wDesiredL, wDesiredR);
+    ROS_INFO("  actual: [%6.1f, %6.1f]", wMeasuredL, wMeasuredR);
+    ROS_INFO("   error: [%6.1f, %6.1f]", errorL, errorR);
+    ROS_INFO("integral: [%6.1f, %6.1f]", motorL.I, motorR.I);
+    ROS_INFO("     PWM: [%6d, %6d]", pwm.PWM1, pwm.PWM2);
   }
   
 private:
-  double last_error1;
-  double last_error2;
-  double I1;
-  double I2;
-  double Kp1;
-  double Ki1;
-  double Kd1;
-  double Kp2;
-  double Ki2;
-  double Kd2;
+  Motor motorL;
+  Motor motorR;
+
   ras_arduino_msgs::PWM pwm;
   ras_arduino_msgs::Encoders enc;
   geometry_msgs::Twist twi;
