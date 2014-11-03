550b61bb717a4bf3306fc14090f1ace26ec84435
ras <ras@RAS24.(none)>
2014-11-03 17:02:48 +0100

    Working adc to distance converter using curve fitting

--- sensors/src/distance_sensors.cpp
+++ sensors/src/distance_sensors.cpp
@@ -8,28 +8,71 @@ ras_arduino_msgs::ADConverter in;
 
 ros::Publisher pub;
 
-float poly(int ch, float p1, float p2, float p3, float p4, float p5, float p6){
-    return p1*pow(ch,5) + p2*pow(ch,4) + p3*pow(ch,3) + p4*pow(ch,2) + p5*ch + p6;
+struct param_t {
+  double p1;
+  double p2;
+  double p3;
+  double p4;
+  double p5;
+  double p6;
+} param_ch1, param_ch2, param_ch3, param_ch4, param_ch5, param_ch6;
+
+void init() {
+  param_ch1.p1 = -1.337e-10;
+  param_ch1.p2 = 1.445e-07;
+  param_ch1.p3 = -6.21e-05;
+  param_ch1.p4 = 0.0135;
+  param_ch1.p5 = -1.572;
+  param_ch1.p6 = 87.45;
+
+  param_ch2.p1 = -3.844e-11;
+  param_ch2.p2 = 6.21e-08;
+  param_ch2.p3 = -3.971e-05;
+  param_ch2.p4 = 0.01279;
+  param_ch2.p5 = -2.171;
+  param_ch2.p6 = 170.5;
+
+  param_ch3.p1 = -1.404e-10;
+  param_ch3.p2 = 1.598e-07;
+  param_ch3.p3 = -7.228e-05;
+  param_ch3.p4 = 0.01651;
+  param_ch3.p5 = -1.993;
+  param_ch3.p6 = 111;
+
+  param_ch4.p1 = 4.222e-11;
+  param_ch4.p2 = -4.48e-08;
+  param_ch4.p3 = 1.686e-05;
+  param_ch4.p4 = -0.002318;
+  param_ch4.p5 = -0.06936;
+  param_ch4.p6 = 35.5;
+
+  param_ch5.p1 = -3.839e-11;
+  param_ch5.p2 = 6.468e-08;
+  param_ch5.p3 = -4.329e-05;
+  param_ch5.p4 = 0.01456;
+  param_ch5.p5 = -2.545;
+  param_ch5.p6 = 198.9;
+
+  param_ch6.p1 = -2.565e-10;
+  param_ch6.p2 = 2.735e-07;
+  param_ch6.p3 = -0.0001143;
+  param_ch6.p4 = 0.02371;
+  param_ch6.p5 = -2.533;
+  param_ch6.p6 = 122.1;
+}
+
+float poly(int ch, param_t params){
+    return params.p1*pow(ch,5) + params.p2*pow(ch,4) + params.p3*pow(ch,3) + params.p4*pow(ch,2) + params.p5*ch + params.p6;
 }
 
 void adcCallback(const ras_arduino_msgs::ADConverter::ConstPtr &msg)
 {
-  in.ch1 += msg->ch1;
-  in.ch2 += msg->ch2;
-  in.ch3 += msg->ch3;
-  in.ch4 += msg->ch4;
-  in.ch5 += msg->ch5;
-  in.ch6 += msg->ch6;
-  in.ch7 += msg->ch7;
-  in.ch8 += msg->ch8;
-
-
-  out.left_front = poly(in.ch1, -0.2579, 0.839, -0.9278, 1.722, -5.946, 7.166);
-  out.front = poly(in.ch2, -0.5123, 3.039, -6.709, 9.331, -15.43, 17.61);
-  out.left_rear = poly(in.ch3, -0.322, 1.018, -1.076, 2.008, -6.101, 6.938);
-  out.right_rear = poly(in.ch4, 0.09064, -0.1877, -0.703, 2.854, -6.393, 7.013);
-  out.rear = poly(in.ch5, -0.371, 2.583, -6.9, 10.51, -15.56, 17.27);
-  out.right_front = poly(in.ch6, -0.3894, 1.293, -1.416, 1.936, -5.762, 6.9);
+  out.left_front = poly(msg->ch1, param_ch1);
+  out.front = poly(msg->ch2, param_ch2);
+  out.left_rear = poly(msg->ch3, param_ch3);
+  out.right_rear = poly(msg->ch4, param_ch4);
+  out.rear = poly(msg->ch5, param_ch5);
+  out.right_front = poly(msg->ch6, param_ch6);
 
 
   pub.publish(out);
@@ -41,7 +84,7 @@ int main (int argc, char** argv)
   // Initialize ROS
   ros::init (argc, argv, "in_topic");
   ros::NodeHandle nh;
-
+  init();
   ros::Subscriber sub = nh.subscribe ("/arduino/adc", 1, adcCallback);
 
 
