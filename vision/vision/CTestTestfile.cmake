744a679048cab16948c19d399e3117a3ffb24ed8
Björn Holm <bjh@kth.se>
2014-11-13 01:34:40 +0100

    made vision into a metapackage

--- vision/vision/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/john/catkin_ws/src/vision/vision
-# Build directory: /home/john/catkin_ws/src/vision/vision
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
