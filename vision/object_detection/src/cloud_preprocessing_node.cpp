1686673f8ce0179a78e1caf26021ffc9a7de44a8
acevail <greimel@kth.se>
2014-11-21 14:41:03 +0100

    Refactored object detection Splitted into cloud preprocessing and object detection

---
+++ vision/object_detection/src/cloud_preprocessing_node.cpp
@@ -0,0 +1,148 @@
+#include <ros/ros.h>
+
+#include <vision_msgs/PreprocessedClouds.h>
+#include <sensor_msgs/PointCloud2.h>
+
+
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/point_cloud.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/crop_box.h>
+#include <pcl/filters/project_inliers.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+
+typedef pcl::PointXYZRGB PointT;
+
+ros::Publisher pub_plane;
+ros::Publisher pub_others;
+ros::Publisher pub_preprocessed;
+
+void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
+{
+    pcl::PointCloud<PointT>::Ptr cloud(new pcl::PointCloud<PointT>());
+
+    // Convert to PCL data type
+    pcl::fromROSMsg(*cloud_msg, *cloud);
+
+    if(cloud->size() == 0)
+        return;
+
+    /** Segmentation */
+    // All the objects needed
+    pcl::PassThrough<PointT> pass;
+    pcl::VoxelGrid<PointT> vgrid;
+    pcl::StatisticalOutlierRemoval<PointT> sor;
+    pcl::NormalEstimation<PointT, pcl::Normal> ne;
+    pcl::SACSegmentationFromNormals<PointT, pcl::Normal> seg;
+    pcl::ExtractIndices<PointT> extract;
+    pcl::ExtractIndices<pcl::Normal> extract_normals;
+    pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT> ());
+
+    // Datasets
+    pcl::PointCloud<PointT>::Ptr cloud_downsampled (new pcl::PointCloud<PointT>);
+    pcl::PointCloud<PointT>::Ptr cloud_cleaned (new pcl::PointCloud<PointT>);
+    pcl::PointCloud<PointT>::Ptr cloud_filtered (new pcl::PointCloud<PointT>);
+    pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
+    pcl::PointCloud<PointT>::Ptr cloud_filtered2 (new pcl::PointCloud<PointT>);
+    pcl::PointCloud<pcl::Normal>::Ptr cloud_normals2 (new pcl::PointCloud<pcl::Normal>);
+
+    pcl::PointCloud<PointT>::Ptr cloud_plane (new pcl::PointCloud<PointT>);
+    pcl::ModelCoefficients::Ptr coefficients_plane (new pcl::ModelCoefficients);
+    pcl::PointIndices::Ptr inliers_plane (new pcl::PointIndices);
+
+    // Create the filtering object
+    vgrid.setInputCloud (cloud);
+    vgrid.setLeafSize (0.01f, 0.01f, 0.01f);
+    vgrid.filter (*cloud_downsampled);
+
+    // Build a passthrough filter to remove spurious NaNs
+    pass.setInputCloud (cloud_downsampled);
+    pass.setFilterFieldName ("z");
+    pass.setFilterLimits (0, 1.5);
+    pass.filter (*cloud_filtered);
+
+    // Estimate point normals
+    ne.setSearchMethod (tree);
+    ne.setInputCloud (cloud_filtered);
+    ne.setKSearch (50);
+    ne.compute (*cloud_normals);
+
+    // Create the segmentation object for the planar model and set all the parameters
+    seg.setOptimizeCoefficients (true);
+    seg.setModelType (pcl::SACMODEL_NORMAL_PLANE);
+    seg.setNormalDistanceWeight (0.1);
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setMaxIterations (100);
+    seg.setDistanceThreshold (0.03);
+    seg.setInputCloud (cloud_filtered);
+    seg.setInputNormals (cloud_normals);
+    // Obtain the plane inliers and coefficients
+    seg.segment (*inliers_plane, *coefficients_plane);
+
+
+    // Extract the planar inliers from the input cloud
+    extract.setInputCloud (cloud_filtered);
+    extract.setIndices (inliers_plane);
+    extract.setNegative (false);
+    extract.filter(*cloud_plane);
+
+    // Remove the planar inliers, extract the rest
+    extract.setNegative (true);
+    extract.filter (*cloud_cleaned);
+
+    extract_normals.setNegative (true);
+    extract_normals.setInputCloud (cloud_normals);
+    extract_normals.setIndices (inliers_plane);
+    extract_normals.filter (*cloud_normals2);
+
+    sor.setInputCloud (cloud_cleaned);
+    sor.setMeanK (50);
+    sor.setStddevMulThresh (1.0);
+    //sor.filter (*cloud_projected);
+    sor.filter (*cloud_filtered2);
+
+    // Convert to ROS data type
+    sensor_msgs::PointCloud2 output_plane;
+    pcl::toROSMsg(*cloud_plane, output_plane);
+    pub_plane.publish(output_plane);
+
+    sensor_msgs::PointCloud2 output_others;
+    pcl::toROSMsg(*cloud_filtered2, output_others);
+    pub_others.publish(output_others);
+
+    vision_msgs::PreprocessedClouds output_preprocessed;
+    output_preprocessed.plane = output_plane;
+    output_preprocessed.others = output_others;
+    pub_preprocessed.publish(output_preprocessed);
+}
+
+int main (int argc, char** argv)
+{
+    // Initialize ROS
+    ros::init (argc, argv, "cloud_preprocessing_node");
+    ros::NodeHandle nh;
+
+    // Create a ROS subscriber for the input point cloud
+    ros::Subscriber sub = nh.subscribe ("/camera/depth_registered/points", 1, cloud_cb);
+
+
+    // Create a ROS publisher for the output point cloud
+    pub_plane = nh.advertise<sensor_msgs::PointCloud2> ("/object_detection/plane", 1);
+    pub_others = nh.advertise<sensor_msgs::PointCloud2> ("/object_detection/others", 1);
+    pub_preprocessed = nh.advertise<vision_msgs::PreprocessedClouds> ("/object_detection/preprocessed", 1);
+
+    ros::spin();
+    return 0;
+}
--- vision/object_detection/src/object_detection_node.cpp
+++ vision/object_detection/src/object_detection_node.cpp
@@ -1,197 +1,152 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
 #include <tf/transform_broadcaster.h>
 
 #include <vision_msgs/Object.h>
 #include <vision_msgs/Objects.h>
+#include <vision_msgs/PreprocessedClouds.h>
 
-#include <pcl/filters/voxel_grid.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl/point_cloud.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/ModelCoefficients.h>
-#include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl/filters/passthrough.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
-#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl_conversions/pcl_conversions.h>
 
-#include <pcl/features/normal_3d.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/project_inliers.h>
+#include <pcl/filters/crop_box.h>
+
 #include <pcl/kdtree/kdtree.h>
 #include <pcl/segmentation/extract_clusters.h>
 
 typedef pcl::PointXYZRGB PointT;
 
-ros::Publisher pub_plane;
+visualization_msgs::Marker points;
+visualization_msgs::Marker walls;
+
 ros::Publisher pub_objects;
 ros::Publisher marker_pub;
 
-void
-cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
+void preprocessed_cb (const vision_msgs::PreprocessedCloudsConstPtr& preprocessed_msg)
 {
-    //ROS_INFO("Start of Callback");
-    // Container for original & filtered data
-    pcl::PointCloud<PointT>::Ptr cloud(new pcl::PointCloud<PointT>());
-    //pcl::PointCloud<PointT>::Ptr cloud_f(new pcl::PointCloud<PointT>());//, cloud_p (new pcl::PointCloud<pcl::PointT>), cloud_f (new pcl::PointCloud<pcl::PointT>);
+    pcl::PointCloud<PointT>::Ptr cloud_plane (new pcl::PointCloud<PointT>);
+    pcl::PointCloud<PointT>::Ptr cloud_others (new pcl::PointCloud<PointT>);
 
-    // Convert to PCL data type
-    pcl::fromROSMsg(*cloud_msg, *cloud);
-
-    if(cloud->size() == 0)
-        return;
-
-    /** Send robot to camera_link transform */
-    //ros::Time now = ros::Time::now();
-    //static tf::TransformBroadcaster br;
-    //tf::Transform transform;
-    //transform.setOrigin(tf::Vector3(0.0, 0.0, 0.3));
-    //tf::Quaternion q;
-    //q.setRPY(-3*M_PI/180, 20*M_PI/180, 0);
-    //transform.setRotation(q);
-    //br.sendTransform(tf::StampedTransform(transform, now, "robot", "camera_link"));
-
-    /** Set up markers */
-    visualization_msgs::Marker points;
-    points.header.frame_id = "/camera_depth_optical_frame";
-    points.header.stamp = cloud_msg->header.stamp;
-    points.ns = "points";
-    points.action = visualization_msgs::Marker::ADD;
-    points.pose.orientation.w = 1.0;
-
-    points.id = 0;
-    points.type = visualization_msgs::Marker::POINTS;
-
-    points.scale.x = 0.05 ;
-    points.scale.y = 0.05;
-    points.color.g = 1.0f;
-    points.color.a = 1.0;
-
-    /** Segmentation */
-    // All the objects needed
-    pcl::PassThrough<PointT> pass;
-    pcl::VoxelGrid<PointT> sor;
-    pcl::NormalEstimation<PointT, pcl::Normal> ne;
-    pcl::SACSegmentationFromNormals<PointT, pcl::Normal> seg;
-    pcl::ExtractIndices<PointT> extract;
-    pcl::ExtractIndices<pcl::Normal> extract_normals;
     pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT> ());
 
-    // Datasets
-    pcl::PointCloud<PointT>::Ptr cloud_downsampled (new pcl::PointCloud<PointT>);
-    pcl::PointCloud<PointT>::Ptr cloud_filtered (new pcl::PointCloud<PointT>);
-    pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
-    pcl::PointCloud<PointT>::Ptr cloud_filtered2 (new pcl::PointCloud<PointT>);
-    pcl::PointCloud<pcl::Normal>::Ptr cloud_normals2 (new pcl::PointCloud<pcl::Normal>);
-    pcl::ModelCoefficients::Ptr coefficients_plane (new pcl::ModelCoefficients);
-    pcl::PointIndices::Ptr inliers_plane (new pcl::PointIndices);
+    points.header.stamp = walls.header.stamp= preprocessed_msg->plane.header.stamp;
+    points.points.clear();
+    walls.points.clear();
 
-    // Create the filtering object
-    sor.setInputCloud (cloud);
-    sor.setLeafSize (0.01f, 0.01f, 0.01f);
-    sor.filter (*cloud_downsampled);
+    pcl::fromROSMsg(preprocessed_msg->plane, *cloud_plane);
+    pcl::fromROSMsg(preprocessed_msg->others, *cloud_others);
 
-    // Build a passthrough filter to remove spurious NaNs
-    pass.setInputCloud (cloud_downsampled);
-    pass.setFilterFieldName ("z");
-    pass.setFilterLimits (0, 1.5);
-    pass.filter (*cloud_filtered);
-    //std::cerr << "PointCloud after filtering has: " << cloud_filtered->points.size () << " data points." << std::endl;
-
-    // Estimate point normals
-    ne.setSearchMethod (tree);
-    ne.setInputCloud (cloud_filtered);
-    ne.setKSearch (50);
-    ne.compute (*cloud_normals);
-
-    // Create the segmentation object for the planar model and set all the parameters
-    seg.setOptimizeCoefficients (true);
-    seg.setModelType (pcl::SACMODEL_NORMAL_PLANE);
-    seg.setNormalDistanceWeight (0.1);
-    seg.setMethodType (pcl::SAC_RANSAC);
-    seg.setMaxIterations (100);
-    seg.setDistanceThreshold (0.03);
-    seg.setInputCloud (cloud_filtered);
-    seg.setInputNormals (cloud_normals);
-    // Obtain the plane inliers and coefficients
-    seg.segment (*inliers_plane, *coefficients_plane);
-    //std::cerr << "Plane coefficients: " << *coefficients_plane << std::endl;
-
-    // Extract the planar inliers from the input cloud
-    extract.setInputCloud (cloud_filtered);
-    extract.setIndices (inliers_plane);
-    extract.setNegative (false);
-
-    // Remove the planar inliers, extract the rest
-    extract.setNegative (true);
-    extract.filter (*cloud_filtered2);
-    extract_normals.setNegative (true);
-    extract_normals.setInputCloud (cloud_normals);
-    extract_normals.setIndices (inliers_plane);
-    extract_normals.filter (*cloud_normals2);
-
-    if(cloud_filtered2->size() == 0)
+    if(cloud_plane->size() == 0 || cloud_others->size() == 0)
         return;
-    // Creating the KdTree object for the search method of the extraction
-    tree->setInputCloud (cloud_filtered2);
 
+    //Projects points onto floor
+    /*pcl::ProjectInliers<PointT> proj;
+    proj.setModelType (pcl::SACMODEL_PLANE);
+    proj.setModelCoefficients (coefficients_plane);
+    proj.setInputCloud (cloud_filtered2);
+    proj.filter (*cloud_projected);*/
+
+    //Generate clusters
     std::vector<pcl::PointIndices> cluster_indices;
     pcl::EuclideanClusterExtraction<PointT> ec;
-    ec.setClusterTolerance (0.04); // 2cm
+    ec.setClusterTolerance (0.02);
     ec.setMinClusterSize (10);
-    ec.setMaxClusterSize (100);
+    ec.setMaxClusterSize (150);
+    //tree->setInputCloud (cloud_others);
     ec.setSearchMethod (tree);
-    ec.setInputCloud (cloud_filtered2);
+    ec.setInputCloud (cloud_others);
     ec.extract (cluster_indices);
 
+    tree->setInputCloud(cloud_plane);
+    std::vector<int> pointIdxNKNSearch(1);
+    std::vector<float> pointNKNSquaredDistance(1);
 
     int j = 0;
     vision_msgs::Objects objects;
     for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
     {
-        pcl::PointCloud<PointT>::Ptr cloud_cluster (new pcl::PointCloud<PointT>(*cloud_filtered2, it->indices));
-        //pcl::toROSMsg(*cloud_cluster, output_object);
+        pcl::PointCloud<PointT>::Ptr cloud_cluster (new pcl::PointCloud<PointT>(*cloud_others, it->indices));
 
         Eigen::Vector4f centroid;
-        pcl::compute3DCentroid(*cloud_filtered2, it->indices, centroid);
+        pcl::compute3DCentroid(*cloud_others, it->indices, centroid);
 
-        geometry_msgs::Point p;
-        p.x = centroid[0];
-        p.y = centroid[1];
-        p.z = centroid[2];
-        points.points.push_back(p);
+        PointT centroid_point;
+        centroid_point.x = centroid[0];
+        centroid_point.y = centroid[1];
+        centroid_point.z = centroid[2];
 
+        if(tree->nearestKSearch(centroid_point, 1,  pointIdxNKNSearch, pointNKNSquaredDistance) > 0 ) {
+
+            if(pointNKNSquaredDistance[0] > 0.05) {
+
+                //extract.setIndices(it->indices);
+                //extract.filter(*cloud_filtered2);
+
+                //std::cout << "Distance:" << pointNKNSquaredDistance[0] << std::endl;
+
+                /*geometry_msgs::Point w;
+                w.x = cloud_plane->points[ pointIdxNKNSearch[0] ].x;
+                w.y = cloud_plane->points[ pointIdxNKNSearch[0] ].y;
+                w.z = cloud_plane->points[ pointIdxNKNSearch[0] ].z;
+
+                geometry_msgs::Point w2;
+                w2.x = centroid[0];
+                w2.y = centroid[1];
+                w2.z = centroid[2];
+
+                walls.points.push_back(w);
+                walls.points.push_back(w2);*/
+                break;
+            }
+
+
+        }
+
+        //Get mean color
         Eigen::Vector3f color(0,0,0);
-        for(int k = 0; k < cloud_cluster->size(); k++) {
+        for(int k = 0; k < cloud_cluster->size(); ) {
             Eigen::Vector3i col = cloud_cluster->points[k].getRGBVector3i();
             color[0] += col[0];
             color[1] += col[1];
             color[2] += col[2];
+            k++;
         }
         color[0] /= (float) cloud_cluster->size();
         color[1] /= (float) cloud_cluster->size();
         color[2] /= (float) cloud_cluster->size();
 
+        //Generate output and marker point
+        geometry_msgs::Point p;
         vision_msgs::Object ob;
+
+        p.x = ob.x = centroid[0];
+        p.y = ob.y = centroid[1];
+        p.z = ob.z = centroid[2];
+
         ob.r = color[0];
         ob.g = color[1];
         ob.b = color[2];
+
+        points.points.push_back(p);
         objects.objects.push_back(ob);
 
-        std::cout << "Centroid:" << centroid << std::endl;
-        std::cout << "Color:" << color << std::endl;
+        //std::cout << "Centroid:" << centroid << std::endl;
+        //std::cout << "Color:" << color << std::endl;
         j++;
     }
-    // Convert to ROS data type
-    sensor_msgs::PointCloud2 output;
-    pcl::toROSMsg(*cloud_filtered2, output);
-    pub_plane.publish(output);
 
     pub_objects.publish(objects);
+    std::cout << "Published " << objects.objects.size() << " objects" << std::endl;
 
-    marker_pub.publish(points);
+    visualization_msgs::MarkerArray marker;
+    marker.markers.push_back(points);
+    marker.markers.push_back(walls);
+    marker_pub.publish(marker);
 }
 
 
@@ -202,14 +157,33 @@ int main (int argc, char** argv)
     ros::init (argc, argv, "object_detection_node");
     ros::NodeHandle nh;
 
-    // Create a ROS subscriber for the input point cloud
-    ros::Subscriber sub = nh.subscribe ("/camera/depth_registered/points", 1, cloud_cb);
+    ros::Subscriber sub = nh.subscribe ("/object_detection/preprocessed", 1, preprocessed_cb);
 
-
-    // Create a ROS publisher for the output point cloud
-    pub_plane = nh.advertise<sensor_msgs::PointCloud2> ("/object_detection/plane", 1);
     pub_objects = nh.advertise<vision_msgs::Objects> ("/object_detection/objects", 1);
-    marker_pub = nh.advertise<visualization_msgs::Marker>("visualization_marker", 10);
+    marker_pub = nh.advertise<visualization_msgs::MarkerArray>("/object_detection/markers", 10);
+
+    /** Set up markers */
+    points.header.frame_id = walls.header.frame_id = "/camera_depth_optical_frame";
+    points.ns = walls.ns = "points_and_lines";
+    points.action = walls.action = visualization_msgs::Marker::ADD;
+    points.pose.orientation.w = walls.pose.orientation.w = 1.0;
+
+    points.id = 0;
+    walls.id = 1;
+
+    points.type = visualization_msgs::Marker::POINTS;
+    walls.type = visualization_msgs::Marker::LINE_LIST;
+
+    points.scale.x = 0.05 ;
+    points.scale.y = 0.05;
+
+    walls.scale.x = 0.01;
+
+    points.color.g = 1.0;
+    points.color.a = 1.0;
+
+    walls.color.r = 1.0;
+    walls.color.a = 1.0;
 
 
     ros::spin();
