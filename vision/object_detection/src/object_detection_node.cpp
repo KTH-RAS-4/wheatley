7492e178caea8234206ecfd364cbdb8ad15a0254
acevail <greimel@kth.se>
2014-11-21 17:48:41 +0100

    Object coordinates are transformed into map space. Object detection is represented as a class.

--- vision/object_detection/src/object_detection_node.cpp
+++ vision/object_detection/src/object_detection_node.cpp
@@ -1,7 +1,9 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
-#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+#include <pcl_ros/transforms.h>
+#include <geometry_msgs/PointStamped.h>
 
 #include <vision_msgs/Object.h>
 #include <vision_msgs/Objects.h>
@@ -21,133 +23,195 @@
 
 typedef pcl::PointXYZRGB PointT;
 
-visualization_msgs::Marker points;
-visualization_msgs::Marker walls;
 
-ros::Publisher pub_objects;
-ros::Publisher marker_pub;
+class PreprocessorNode {
 
-void preprocessed_cb (const vision_msgs::PreprocessedCloudsConstPtr& preprocessed_msg)
-{
-    pcl::PointCloud<PointT>::Ptr cloud_plane (new pcl::PointCloud<PointT>);
-    pcl::PointCloud<PointT>::Ptr cloud_others (new pcl::PointCloud<PointT>);
+private:
+    ros::NodeHandle nh;
+    ros::Publisher pub_objects;
+    ros::Publisher marker_pub;
+    ros::Subscriber sub;
 
-    pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT> ());
+    visualization_msgs::Marker points;
+    visualization_msgs::Marker walls;
 
-    points.header.stamp = walls.header.stamp= preprocessed_msg->plane.header.stamp;
-    points.points.clear();
-    walls.points.clear();
+    tf::TransformListener tfl;
 
-    pcl::fromROSMsg(preprocessed_msg->plane, *cloud_plane);
-    pcl::fromROSMsg(preprocessed_msg->others, *cloud_others);
+public:
+    PreprocessorNode() {
+        ros::Time now(0);
 
-    if(cloud_plane->size() == 0 || cloud_others->size() == 0)
-        return;
+        while (!tfl.waitForTransform("camera_depth_optical_frame", "world", now, ros::Duration(1)))
+            ROS_ERROR("Couldn't find transform from 'robot' to 'ir_front', retrying...");
 
-    //Projects points onto floor
-    /*pcl::ProjectInliers<PointT> proj;
-    proj.setModelType (pcl::SACMODEL_PLANE);
-    proj.setModelCoefficients (coefficients_plane);
-    proj.setInputCloud (cloud_filtered2);
-    proj.filter (*cloud_projected);*/
+        sub = nh.subscribe ("/object_detection/preprocessed", 1, &PreprocessorNode::preprocessed_cb, this);
 
-    //Generate clusters
-    std::vector<pcl::PointIndices> cluster_indices;
-    pcl::EuclideanClusterExtraction<PointT> ec;
-    ec.setClusterTolerance (0.02);
-    ec.setMinClusterSize (10);
-    ec.setMaxClusterSize (150);
-    //tree->setInputCloud (cloud_others);
-    ec.setSearchMethod (tree);
-    ec.setInputCloud (cloud_others);
-    ec.extract (cluster_indices);
-
-    tree->setInputCloud(cloud_plane);
-    std::vector<int> pointIdxNKNSearch(1);
-    std::vector<float> pointNKNSquaredDistance(1);
-
-    int j = 0;
-    vision_msgs::Objects objects;
-    for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
-    {
-        pcl::PointCloud<PointT>::Ptr cloud_cluster (new pcl::PointCloud<PointT>(*cloud_others, it->indices));
-
-        Eigen::Vector4f centroid;
-        pcl::compute3DCentroid(*cloud_others, it->indices, centroid);
-
-        PointT centroid_point;
-        centroid_point.x = centroid[0];
-        centroid_point.y = centroid[1];
-        centroid_point.z = centroid[2];
-
-        if(tree->nearestKSearch(centroid_point, 1,  pointIdxNKNSearch, pointNKNSquaredDistance) > 0 ) {
-
-            if(pointNKNSquaredDistance[0] > 0.05) {
-
-                //extract.setIndices(it->indices);
-                //extract.filter(*cloud_filtered2);
-
-                //std::cout << "Distance:" << pointNKNSquaredDistance[0] << std::endl;
-
-                /*geometry_msgs::Point w;
-                w.x = cloud_plane->points[ pointIdxNKNSearch[0] ].x;
-                w.y = cloud_plane->points[ pointIdxNKNSearch[0] ].y;
-                w.z = cloud_plane->points[ pointIdxNKNSearch[0] ].z;
-
-                geometry_msgs::Point w2;
-                w2.x = centroid[0];
-                w2.y = centroid[1];
-                w2.z = centroid[2];
-
-                walls.points.push_back(w);
-                walls.points.push_back(w2);*/
-                break;
-            }
+        pub_objects = nh.advertise<vision_msgs::Objects> ("/object_detection/objects", 1);
+        marker_pub = nh.advertise<visualization_msgs::MarkerArray>("/object_detection/markers", 10);
 
 
-        }
+        /** Set up markers */
+        points.header.frame_id = walls.header.frame_id = "/map";
+        points.ns = walls.ns = "points_and_lines";
+        points.action = walls.action = visualization_msgs::Marker::ADD;
+        points.pose.orientation.w = walls.pose.orientation.w = 1.0;
 
-        //Get mean color
-        Eigen::Vector3f color(0,0,0);
-        for(int k = 0; k < cloud_cluster->size(); ) {
-            Eigen::Vector3i col = cloud_cluster->points[k].getRGBVector3i();
-            color[0] += col[0];
-            color[1] += col[1];
-            color[2] += col[2];
-            k++;
-        }
-        color[0] /= (float) cloud_cluster->size();
-        color[1] /= (float) cloud_cluster->size();
-        color[2] /= (float) cloud_cluster->size();
+        points.id = 0;
+        walls.id = 1;
 
-        //Generate output and marker point
-        geometry_msgs::Point p;
-        vision_msgs::Object ob;
+        points.type = visualization_msgs::Marker::POINTS;
+        walls.type = visualization_msgs::Marker::LINE_LIST;
 
-        p.x = ob.x = centroid[0];
-        p.y = ob.y = centroid[1];
-        p.z = ob.z = centroid[2];
+        points.scale.x = 0.05 ;
+        points.scale.y = 0.05;
 
-        ob.r = color[0];
-        ob.g = color[1];
-        ob.b = color[2];
+        walls.scale.x = 0.01;
 
-        points.points.push_back(p);
-        objects.objects.push_back(ob);
+        points.color.g = 1.0;
+        points.color.a = 1.0;
 
-        //std::cout << "Centroid:" << centroid << std::endl;
-        //std::cout << "Color:" << color << std::endl;
-        j++;
+        walls.color.r = 1.0;
+        walls.color.a = 1.0;
     }
 
-    pub_objects.publish(objects);
-    std::cout << "Published " << objects.objects.size() << " objects" << std::endl;
+    ~PreprocessorNode() {
 
-    visualization_msgs::MarkerArray marker;
-    marker.markers.push_back(points);
-    marker.markers.push_back(walls);
-    marker_pub.publish(marker);
-}
+    }
+
+    void preprocessed_cb (const vision_msgs::PreprocessedCloudsConstPtr& preprocessed_msg)
+    {
+
+        pcl::PointCloud<PointT>::Ptr cloud_plane (new pcl::PointCloud<PointT>);
+        pcl::PointCloud<PointT>::Ptr cloud_others (new pcl::PointCloud<PointT>);
+
+        pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT> ());
+
+        points.header.stamp = walls.header.stamp= preprocessed_msg->plane.header.stamp;
+        points.points.clear();
+        walls.points.clear();
+
+        //tfl.lookupTransform("world", "camera_depth_optical_frame", preprocessed_msg->plane.header.stamp, map_transform);
+
+
+
+        pcl::fromROSMsg(preprocessed_msg->plane, *cloud_plane);
+        pcl::fromROSMsg(preprocessed_msg->others, *cloud_others);
+
+        if(cloud_plane->size() == 0 || cloud_others->size() == 0)
+            return;
+
+        //Projects points onto floor
+        /*pcl::ProjectInliers<PointT> proj;
+        proj.setModelType (pcl::SACMODEL_PLANE);
+        proj.setModelCoefficients (coefficients_plane);
+        proj.setInputCloud (cloud_filtered2);
+        proj.filter (*cloud_projected);*/
+
+        //Generate clusters
+        std::vector<pcl::PointIndices> cluster_indices;
+        pcl::EuclideanClusterExtraction<PointT> ec;
+        ec.setClusterTolerance (0.02);
+        ec.setMinClusterSize (10);
+        ec.setMaxClusterSize (150);
+        //tree->setInputCloud (cloud_others);
+        ec.setSearchMethod (tree);
+        ec.setInputCloud (cloud_others);
+        ec.extract (cluster_indices);
+
+        tree->setInputCloud(cloud_plane);
+        std::vector<int> pointIdxNKNSearch(1);
+        std::vector<float> pointNKNSquaredDistance(1);
+
+        int j = 0;
+        vision_msgs::Objects objects;
+        for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+        {
+            pcl::PointCloud<PointT>::Ptr cloud_cluster (new pcl::PointCloud<PointT>(*cloud_others, it->indices));
+
+            Eigen::Vector4f centroid;
+            pcl::compute3DCentroid(*cloud_others, it->indices, centroid);
+
+            PointT centroid_point;
+            centroid_point.x = centroid[0];
+            centroid_point.y = centroid[1];
+            centroid_point.z = centroid[2];
+
+            if(tree->nearestKSearch(centroid_point, 1,  pointIdxNKNSearch, pointNKNSquaredDistance) > 0 ) {
+
+                if(pointNKNSquaredDistance[0] > 0.05) {
+
+                    //std::cout << "Distance:" << pointNKNSquaredDistance[0] << std::endl;
+
+                    /*geometry_msgs::Point w;
+                    w.x = cloud_plane->points[ pointIdxNKNSearch[0] ].x;
+                    w.y = cloud_plane->points[ pointIdxNKNSearch[0] ].y;
+                    w.z = cloud_plane->points[ pointIdxNKNSearch[0] ].z;
+
+                    geometry_msgs::Point w2;
+                    w2.x = centroid[0];
+                    w2.y = centroid[1];
+                    w2.z = centroid[2];
+
+                    walls.points.push_back(w);
+                    walls.points.push_back(w2);*/
+                    break;
+                }
+            }
+
+            //Get mean color
+            Eigen::Vector3f color(0,0,0);
+            for(int k = 0; k < cloud_cluster->size(); ) {
+                Eigen::Vector3i col = cloud_cluster->points[k].getRGBVector3i();
+                color[0] += col[0];
+                color[1] += col[1];
+                color[2] += col[2];
+                k++;
+            }
+            color[0] /= (float) cloud_cluster->size();
+            color[1] /= (float) cloud_cluster->size();
+            color[2] /= (float) cloud_cluster->size();
+
+            geometry_msgs::PointStamped p;
+            geometry_msgs::PointStamped p_transformed;
+            p.header = preprocessed_msg->plane.header;
+
+
+            //Generate output and marker point
+            vision_msgs::Object ob;
+
+            p.point.x = centroid[0];
+            p.point.y = centroid[1];
+            p.point.z = centroid[2];
+
+            tfl.transformPoint("map", p, p_transformed);
+
+            ob.x = p_transformed.point.x;
+            ob.y = p_transformed.point.y;
+            ob.z = p_transformed.point.z;
+            ob.r = color[0];
+            ob.g = color[1];
+            ob.b = color[2];
+
+            points.points.push_back(p_transformed.point);
+            objects.objects.push_back(ob);
+
+            //std::cout << "Centroid:" << centroid << std::endl;
+            //std::cout << "Color:" << color << std::endl;
+            j++;
+        }
+
+        pub_objects.publish(objects);
+        std::cout << "Published " << objects.objects.size() << " objects" << std::endl;
+
+        visualization_msgs::MarkerArray marker;
+        marker.markers.push_back(points);
+        marker.markers.push_back(walls);
+        marker_pub.publish(marker);
+    }
+    void run() {
+        ros::spin();
+    }
+};
 
 
 
@@ -155,38 +219,8 @@ int main (int argc, char** argv)
 {
     // Initialize ROS
     ros::init (argc, argv, "object_detection_node");
-    ros::NodeHandle nh;
-
-    ros::Subscriber sub = nh.subscribe ("/object_detection/preprocessed", 1, preprocessed_cb);
-
-    pub_objects = nh.advertise<vision_msgs::Objects> ("/object_detection/objects", 1);
-    marker_pub = nh.advertise<visualization_msgs::MarkerArray>("/object_detection/markers", 10);
-
-    /** Set up markers */
-    points.header.frame_id = walls.header.frame_id = "/camera_depth_optical_frame";
-    points.ns = walls.ns = "points_and_lines";
-    points.action = walls.action = visualization_msgs::Marker::ADD;
-    points.pose.orientation.w = walls.pose.orientation.w = 1.0;
-
-    points.id = 0;
-    walls.id = 1;
-
-    points.type = visualization_msgs::Marker::POINTS;
-    walls.type = visualization_msgs::Marker::LINE_LIST;
-
-    points.scale.x = 0.05 ;
-    points.scale.y = 0.05;
-
-    walls.scale.x = 0.01;
-
-    points.color.g = 1.0;
-    points.color.a = 1.0;
-
-    walls.color.r = 1.0;
-    walls.color.a = 1.0;
-
-
-    ros::spin();
+    PreprocessorNode node;
+    node.run();
     return 0;
 }
 
--- vision/object_recognizer/src/color_detector_node.cpp
+++ vision/object_recognizer/src/color_detector_node.cpp
@@ -1,60 +1,140 @@
 #include <iostream>
-#include <vector>
+#include <list>
 #include <math.h>
 #include <ros/ros.h>
 #include <string>
 #include <vision_msgs/Object.h>
 #include <vision_msgs/Objects.h>
+#include <map>
+
+using namespace std;
 
 float object_colors[] = {
-    200, 180, 180,
+    180, 150, 130,
+    220, 220, 220,
     116.2592, 127.4758, 82.8304,
-    173, 45, 56
+    200, 90, 100,
+    220, 120, 120,
+    250, 150, 140
 };
+
+int relevant_iterations = 10;
 
 std::string color_names[] = {
     "wall",
-    "green",
-    "red"
+    "wall",
+    "green cube",
+    "red cube",
+    "red ball",
+    "patrick"
+};
+
+class StoredObject
+{
+    vision_msgs::Object object;
+    map<int, bool> occ;
+
+public:
+    StoredObject(vision_msgs::Object object, int iteration) {
+        this->object = object;
+        occ[iteration] = true;
+    }
+    ~StoredObject() {
+
+    }
+
+    bool compare(vision_msgs::Object other) {
+        if(abs(other.x - object.x) < 0.01 && abs(other.y - object.y) < 0.01 && abs(other.z - object.z) < 0.01) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    void update(vision_msgs::Object obj, int iteration) {
+        occ[iteration] = true;
+        this->object = obj;
+
+        //Delete old detections
+        if(occ.find(iteration-relevant_iterations) != occ.end())
+            occ.erase(iteration-relevant_iterations);
+    }
+
+    int occurrencyCount(int iteration) {
+        int counter = 0;
+        for(int i = (iteration-(relevant_iterations-1)); i<=iteration; i++) {
+            if(occ.find(i) != occ.end()) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+    vision_msgs::Object getObject() {
+        return this->object;
+    }
 };
 
 class ColorDetectorNode
 {
     ros::NodeHandle handle;
-    ros::Subscriber color_sub;
-
+    ros::Subscriber object_sub;
+    list<StoredObject> object_collector;
+    int iteration;
 
 public:
     ColorDetectorNode()
     {
-        color_sub = handle.subscribe("object_detection/objects", 1, &ColorDetectorNode::colorHandle, this);
-
-
-        std::vector<float> v(3, 3);
-        printVector(v);
+        iteration = 0;
+        object_sub = handle.subscribe("object_detection/objects", 1, &ColorDetectorNode::objectHandle, this);
     }
 
     ~ColorDetectorNode()
     {
     }
 
-    void colorHandle(const vision_msgs::Objects& msg)
+    void objectHandle(const vision_msgs::Objects& msg)
     {
-        std::cout << "---------------------------" << std::endl;
+        int new_objects = 0;
+        //Insert objects into collection
         for (int i = 0; i < msg.objects.size(); i++)
         {
-            int color = findClosestColor(msg.objects[i]);
-            if(color != 0) {
-                std::cout << "Object " << i << ": Color: " << color_names[color] << std::endl;
+            if(storeObject(msg.objects[i]))
+                new_objects++;
+        }
+        cout << "Iteration " << iteration << " - " << new_objects << "/" << msg.objects.size() << " new objects" << endl;
+
+        //Output objects with multiple occurrencies
+
+        for (list<StoredObject>::iterator it = object_collector.begin() ; it != object_collector.end(); ++it) {
+            int obj_iter = 0;
+            if((obj_iter = it->occurrencyCount(iteration)) > (relevant_iterations / 2)) {
+                vision_msgs::Object obj = it->getObject();
+                int color = findClosestColor(obj);
+                if (color_names[color ]!= "wall") {
+                    cout << "Found " << color_names[color] << " (" << obj.r << ", " << obj.g << ", " << obj.b << ")" << endl;
+                } else {
+                    cout << "Found wall (" << obj.r << ", " << obj.g << ", " << obj.b << ")" << endl;
+                }
+            } else if (obj_iter == 0) {
+                it = object_collector.erase(it);
             }
         }
-        if (msg.objects.size() == 0)
-        {
-            std::cout << "" << std::endl;
-        }
-        std::cout << "---------------------------" << std::endl;
 
+        iteration++;
     }
+
+    bool storeObject(vision_msgs::Object obj)
+    {
+        for (list<StoredObject>::iterator it = object_collector.begin() ; it != object_collector.end(); ++it) {
+            if(it->compare(obj)) {
+                it->update(obj, iteration);
+                return false;
+            }
+        }
+        object_collector.push_back(StoredObject(obj, iteration));
+        return true;
+    }
+
+
 
     int findClosestColor(vision_msgs::Object obj)
     {
@@ -74,15 +154,6 @@ public:
         }
 
         return color;
-    }
-
-    void printVector(std::vector<float> v)
-    {
-        for (std::vector<float>::iterator it = v.begin(); it != v.end(); ++it)
-        {
-            std::cout << *it << ' ';
-        }
-        std::cout << std::endl;
     }
 };
 
