316bf6076c7d655cd65d228c61c825b5e595ede6
Bjorn Holm <bjh@kth.se>
2014-11-21 11:30:03 +0100

    added max_distance_cutoff to points in the ir cloud

--- sensors/src/ir_points_node.cpp
+++ sensors/src/ir_points_node.cpp
@@ -19,6 +19,12 @@ private:
     tf::StampedTransform tf_left_rear;
     tf::StampedTransform tf_right_front;
     tf::StampedTransform tf_right_rear;
+    double cutoff_front;
+    double cutoff_rear;
+    double cutoff_left_front;
+    double cutoff_left_rear;
+    double cutoff_right_front;
+    double cutoff_right_rear;
 
 public:
     IrPointsNode()
@@ -37,6 +43,14 @@ public:
         tfl.lookupTransform("robot", "ir_right_front", now, tf_right_front);
         tfl.lookupTransform("robot", "ir_right_rear",  now, tf_right_rear);
 
+        ros::NodeHandle nhp("/sensors/ir/max_distance_cutoff");
+        nhp.getParam("front",       cutoff_front);
+        nhp.getParam("rear",        cutoff_rear);
+        nhp.getParam("left_front",  cutoff_left_front);
+        nhp.getParam("left_rear",   cutoff_left_rear);
+        nhp.getParam("right_front", cutoff_right_front);
+        nhp.getParam("right_rear",  cutoff_right_rear);
+
         pub_ir = nh.advertise<sensor_msgs::PointCloud2>("", 10);
         sub_distance = nh.subscribe ("/sensors/ir/distances", 10, &IrPointsNode::distanceCallback, this);
     }
@@ -48,17 +62,19 @@ public:
 
     void distanceCallback(const sensors::Distance::ConstPtr &distance)
     {
+        std::vector<pcl::PointXYZ> ps;
+        if (distance->front      <= cutoff_front      ) ps.push_back(toPCL(tf_front      *tf::Vector3(distance->front      ,0,0)));
+        if (distance->rear       <= cutoff_rear       ) ps.push_back(toPCL(tf_rear       *tf::Vector3(distance->rear       ,0,0)));
+        if (distance->left_front <= cutoff_left_front ) ps.push_back(toPCL(tf_left_front *tf::Vector3(distance->left_front ,0,0)));
+        if (distance->left_rear  <= cutoff_left_rear  ) ps.push_back(toPCL(tf_left_rear  *tf::Vector3(distance->left_rear  ,0,0)));
+        if (distance->right_front<= cutoff_right_front) ps.push_back(toPCL(tf_right_front*tf::Vector3(distance->right_front,0,0)));
+        if (distance->right_rear <= cutoff_right_rear ) ps.push_back(toPCL(tf_right_rear *tf::Vector3(distance->right_rear ,0,0)));
+
         pcl::PointCloud<pcl::PointXYZ> cloud;
         cloud.width = 1;
-        cloud.height = 6;
-
-        cloud.push_back(toPCL(tf_front       * tf::Vector3(distance->front       , 0, 0)));
-        cloud.push_back(toPCL(tf_rear        * tf::Vector3(distance->rear        , 0, 0)));
-        cloud.push_back(toPCL(tf_left_front  * tf::Vector3(distance->left_front  , 0, 0)));
-        cloud.push_back(toPCL(tf_left_rear   * tf::Vector3(distance->left_rear   , 0, 0)));
-        cloud.push_back(toPCL(tf_right_front * tf::Vector3(distance->right_front , 0, 0)));
-        cloud.push_back(toPCL(tf_right_rear  * tf::Vector3(distance->right_rear  , 0, 0)));
-
+        cloud.height = ps.size();
+        for (int i=0; i<ps.size(); i++)
+            cloud.push_back(ps[i]);
         sensor_msgs::PointCloud2 ros_cloud;
         pcl::toROSMsg(cloud, ros_cloud);
         ros_cloud.header.frame_id = "robot";
