fcb9c299dba4cbc32aff2deef069563346e153e9
Bjorn Holm <bjh@kth.se>
2014-12-01 16:14:07 +0100

    added publishing of FORWARD/LEFT/RIGHT/STOP in xbox_node

--- controllers/xbox/src/xbox_node.cpp
+++ controllers/xbox/src/xbox_node.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
+#include <std_msgs/String.h>
 #include <cmath>
 
 
@@ -9,26 +10,28 @@ class Xbox
 private:
     ros::NodeHandle nh;
     ros::Publisher pub_twist;
+    ros::Publisher pub_direction;
     ros::Subscriber sub_joy;
+    ros::Timer timer_min_rate;
     sensor_msgs::Joy joy;
-    ros::Rate loop_rate;
     double linearConstant;
     double angularConstant;
 
 public:
     Xbox()
         : nh("~")
-        , loop_rate(1)
     {
-        double rate;
-        nh.getParam("rate", rate);
-        loop_rate = ros::Rate(rate);
+        double min_rate;
 
         nh.getParam("linear_constant", linearConstant);
         nh.getParam("angular_constant", angularConstant);
+        nh.getParam("min_rate", min_rate);
 
         pub_twist = nh.advertise<geometry_msgs::Twist>("twist", 1);
+        pub_direction = nh.advertise<std_msgs::String>("direction", 1);
+
         sub_joy = nh.subscribe("joy", 1, &Xbox::joyCallback, this);
+        timer_min_rate = nh.createTimer(ros::Rate(min_rate), &Xbox::timer, this);
     }
 
     ~Xbox()
@@ -41,25 +44,52 @@ public:
         calc();
     }
 
+    void timer(const ros::TimerEvent& time)
+    {
+        calc();
+    }
+
     void calc()
     {
-        geometry_msgs::Twist twist;
+        // publish twist
 
+        static bool stopped = true;
+        geometry_msgs::Twist twist;
         twist.linear.x = linearConstant * joy.axes[1];
         twist.angular.z = angularConstant * joy.axes[3];
 
-        pub_twist.publish(twist);
+        bool stop = twist.linear.x == 0 && twist.angular.y == 0;
+
+        if (!(stop && stopped))
+            pub_twist.publish(twist);
+        stopped = stop;
+
+
+        // publish FORWARD/LEFT/RIGHT
+
+        static std::string prev_cmd = "STOP";
+        std::string cmd;
+        if (joy.buttons[0] || joy.axes[7] == -1)
+            cmd = "STOP";
+        else if (joy.buttons[1] || joy.axes[6] == -1)
+            cmd = "RIGHT";
+        else if (joy.buttons[2] || joy.axes[6] == 1)
+            cmd = "LEFT";
+        else if (joy.buttons[3] || joy.axes[7] == 1)
+            cmd = "FORWARD";
+
+        if (prev_cmd != cmd)
+        {
+            prev_cmd = cmd;
+            std_msgs::String msg_cmd;
+            msg_cmd.data = cmd;
+            pub_direction.publish(msg_cmd);
+        }
     }
 
     void run()
     {
         ros::spin();
-        /*while(ros::ok())
-        {
-          ros::spinOnce();
-          calc();
-          loop_rate.sleep();
-        }*/
     }
 };
 
